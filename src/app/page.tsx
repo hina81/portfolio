c45b9dd25be79a8a53ac06b2ad47e5b67ced92a2
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-03-05 11:40:24 +0900

    add: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,79 +1,89 @@
 "use client";
 import Image from "next/image";
 import Card from "@/components/Card";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <Container>
-      <Box sx={{ marginBottom: 4, marginTop: 4 }}>
-        <Image
-          src="/usagi.png"
-          alt="icon"
-          width={150}
-          height={150}
-          style={{
-            marginBottom: 16,
-            borderRadius: "25%",
-            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-            border: "4px solid #fff",
-          }}
-        />
+    <div className="flex flex-col min-h-screen bg-neutral-50">
+      <Container className="flex-grow">
+        <div className="my-4">
+          <Image
+            src="/usagi.png"
+            alt="icon"
+            width={150}
+            height={150}
+            style={{
+              marginBottom: 16,
+              borderRadius: "50%",
+              boxShadow: "0px 2px 5px rgba(0, 0, 0, 0.2)",
+              border: "4px solid #fff",
+            }}
+          />
 
-        <Typography
-          variant="h5"
-          sx={{ color: "text.secondary", marginBottom: 1, fontSize: "1.6rem" }}
-        >
-          Hi there 👋
-        </Typography>
+          <Typography
+            variant="h5"
+            sx={{
+              marginBottom: 1,
+              fontSize: "1.6rem",
+            }}
+          >
+            Hi there 👋
+          </Typography>
 
-        <Typography
-          variant="body2"
-          sx={{ color: "text.secondary", lineHeight: "1.5em" }}
-        >
-          広島生まれ広島育ちの大学2年生です。
-          <br />
-          Webアプリ開発のエンジニアを目指していて、現在はフロントエンドをメインに勉強しています。
-        </Typography>
-      </Box>
-
-      <Grid container spacing={2}>
-        <Grid item xs={6} sm={4} md={3}>
-          <Card
-            icon={<GitHubIcon sx={{ color: "black" }} />}
-            title="GitHub"
-            link="https://github.com/hina81"
-            subtitle="hina81"
-          />
-        </Grid>
-        <Grid item xs={6} sm={4} md={3}>
-          <Card
-            icon={<Image src="/zenn.svg" alt="Zenn" width={24} height={24} />}
-            title="Zenn"
-            link="https://zenn.dev/hina81"
-            subtitle="hina81"
-          />
-        </Grid>
-        <Grid item xs={6} sm={4} md={3}>
-          <Card
-            icon={<TwitterIcon sx={{ color: "#1DA1F2" }} />}
-            title="Twitter"
-            link="https://twitter.com/hina___9"
-            subtitle="@hina___9"
-          />
-        </Grid>
-        <Grid item xs={6} sm={4} md={3}>
-          <Card
-            icon={<InstagramIcon />}
-            title="Instagram"
-            link="https://www.instagram.com/__hina.811/"
-            subtitle="@___hina.811"
-          />
-        </Grid>
-      </Grid>
-    </Container>
+          <Typography
+            variant="body2"
+            sx={{ color: "text.secondary", lineHeight: "1.7em" }}
+          >
+            広島生まれ広島育ちの大学3年生です。
+            <br />
+            Webアプリ開発のエンジニアを目指していて、現在はフロントエンドをメインに勉強しています。
+          </Typography>
+        </div>
+        <div className="mb-4">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+            <div>
+              <Card
+                icon={<GitHubIcon sx={{ color: "black" }} />}
+                title="GitHub"
+                link="https://github.com/hina81"
+                subtitle="hina81"
+              />
+            </div>
+            <div>
+              <Card
+                icon={
+                  <Image src="/zenn.svg" alt="Zenn" width={24} height={24} />
+                }
+                title="Zenn"
+                link="https://zenn.dev/hina81"
+                subtitle="hina81"
+              />
+            </div>
+            <div>
+              <Card
+                icon={<TwitterIcon sx={{ color: "#1DA1F2" }} />}
+                title="Twitter"
+                link="https://twitter.com/hina___9"
+                subtitle="@hina___9"
+              />
+            </div>
+            <div>
+              <Card
+                icon={<InstagramIcon />}
+                title="Instagram"
+                link="https://www.instagram.com/__hina.811/"
+                subtitle="@___hina.811"
+              />
+            </div>
+          </div>
+        </div>
+      </Container>
+      <Footer />
+    </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="w-full h-24 bg-neutral-400 flex items-center justify-center text-white">
+      <Typography>© 2025 hina81.com</Typography>
+    </footer>
+  );
+}
+
+export default Footer;
